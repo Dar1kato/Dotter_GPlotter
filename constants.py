245799bee2241040dc98f96afa0a3fb187c0c093
Dar1kato <danielpenacruz@gmail.com>
2026-04-20 17:30:01 -0600

    Script Completion

--- constants.py
+++ constants.py
@@ -2,47 +2,19 @@
 # Speeds
 #######
 TRAVEL_SPEED   = 3000  
-WORKING_SPEED  = 500
 
-#######
-# Heights
-#######
-BASE_HEIGHT       =  10.0  
-INSERTION_HEIGHT  =  -5.0 
-TIP_CHANGE_HEIGHT = -12.0  
-SAMPLE_HEIGHT = -5.0
-
-#######
-# Tip box position and spacing
-#######
-TIP_BOX_START_X =  50.0   # medir con jogging
-TIP_BOX_START_Y =   0.0
-TIP_SPACING     =   9.0   # separación entre puntas en el rack (mm)
-
-#######
-# Trash bin position
-#######
-BIN_X = 100.0   # medir con jogging
-BIN_Y =   0.0
-
-#TODO Coordenadas de cada color — medir con jogging
-#TODO El origen [0,0] es el centro del disco Petri
 
 #######
 # Color positions
 #######
-COLOR_POSITIONS = {
-    "red":    (-80.0,   0.0),
-    "blue":   (-80.0,  15.0),
-    "green":  (-80.0,  30.0),
-    "yellow": (-80.0,  45.0),
-    "purple": (-80.0,  60.0),
+COLOR_DICT = {
+    "red":      "S25",
+    "blue":     "S75",
+    "green":    "S50",
+    "yellow":   "S100",
+    "purple":   "S150",
+    "orange":   "S175"
 }
 
-#######
-# Speeds
-#######
-DISC_SPACING = 5.0   
 
-def write(file, line: str) -> None:
-    file.write(line + "\n")
+TIME = 30
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import time
 from constants import *
-from pipette import pipette_press, pipette_release
-from tip import tip_change
-from sample import get_sample
 
 def config() -> list:
     """
@@ -10,9 +8,9 @@ def config() -> list:
     Returns:
         list: list containing gcode
     """
-    GCODE: list = ["G21 ; Unidades en milímetros", "G17 ; Plano XY", "G90 ; Coordenadas absolutas",
-                   "G94 ; Feed rate en unidades/minuto", "G28 ; Homing",
-                   f"G0 Z{BASE_HEIGHT:.3f} F{TRAVEL_SPEED} ; Subir a altura segura"]
+    GCODE: list = ["G21 ; Units as mm", "G17 ; XY Plane", "G90 ; Absolute Coordinates",
+                   "G94 ; Feed rate in units/minute", "G28 ; Homing"]
+    
 
     return GCODE
 
@@ -29,37 +27,21 @@ def translate_coordinates(data: dict) -> list:
     """
     
     gcode_local: list = []      # Current gcode list
-    tip_index: int = 0          # Index for tip changes
     
-    for color, points in data.items():
-        gcode_local.append(f"\n; ========== COLOR: {color.upper()} ==========")
+    start = time.time()
+    while time.time() - start < TIME:
         
-        # Handles each point movement and insertion, adds its gcode to the current list
-        for x, y in points:
+        for color, points in data.items():
+            gcode_local.append(f"M3 {COLOR_DICT[color]} ; Color: {color}");
+
+            for x, y in points:
+                gcode_local.append(f"G0 X{x:.3f} Y{y:.3f} F{TRAVEL_SPEED}")
             
-            # Tip change logic, happens once per point
-            gcode_local.extend(tip_change(tip_index)) 
-            tip_index += 1
-        
-            # Sample loading logic, happens once per point
-            gcode_local.extend(get_sample(color))
-            
-            real_x = x * DISC_SPACING
-            real_y = y * DISC_SPACING
-            
-            gcode_local.append(f"G0 X{real_x:.3f} Y{real_y:.3f} F{TRAVEL_SPEED}")
-            gcode_local.append(f"G1 Z{INSERTION_HEIGHT:.3f} F{WORKING_SPEED}")
-            
-            # Pipette press logic
-            gcode_local.extend(pipette_press()) 
-            
-            gcode_local.append(f"G1 Z{BASE_HEIGHT:.3f} F{WORKING_SPEED}")
-            
-            # Pipette release logic
-            gcode_local.extend(pipette_release()) 
 
     # Program end logic
     gcode_local.append(f"\n; --- Fin del programa ---")
+    gcode_local.append("M3 S0")
+    gcode_local.append("G28 ; Home")
     gcode_local.append("M2")
     
     return gcode_local
