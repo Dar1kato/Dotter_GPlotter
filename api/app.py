301565f09f0d54d0c475318d99b8f72e37fc3082
Dar1kato <danielpenacruz@gmail.com>
2026-04-20 18:06:02 -0600

    Vercel Issues Solved

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_file, render_template_string
+from flask import Flask, request, send_file, render_template_string, Response
 import io
 from main import main_stream
 
